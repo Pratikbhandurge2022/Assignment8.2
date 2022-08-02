c78a86c326f52ca97fae7fe155d9aa924dd13402
pratik <pratikbhandurge2222@gmail.com>
2022-08-02 12:33:54 +0530

    UC1Solution

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program!");
+            Console.WriteLine("===========UC1===========");
+            Length UC1 = new Length();
+            UC1.length();
+        }
+    }
+}
